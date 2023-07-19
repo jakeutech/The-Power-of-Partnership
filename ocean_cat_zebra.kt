1853e3333dcc1c239ae932f3e8a10a0d27e42967
jakeutech <136701401+jakeutech@users.noreply.github.com>
2023-07-19 17:57:04 +0700

    Update user permissions

---
+++ ocean_cat_zebra.kt
@@ -0,0 +1,37 @@
+// This program is written in Kotlin to illustrate the power of partnership.
+
+// Declaration of Variables 
+val numFriend = 3
+val numPartners = 5
+val message = "Partnership is a great power."
+
+// Main program starts here 
+fun main() {
+    // Welcome message 
+    println("Welcome to 'The Power of Partnership' program!")
+    
+    // Calculate the total number of friends and partners 
+    val totNum = numFriend + numPartners
+    println("The total number of friends and partners is: $totNum")
+    
+    // Demonstrate how friendship and partnership can be constructive 
+    println("Friendship and partnership can be constructive.")
+    println("With friendship, we can share our joys and sadness.")
+    println("With partnership, we can build up our strength to move ahead.")
+    
+    // Demonstrate the importance of friendship and partnership 
+    println("Friendship and partnership are both essential to build trust and understanding.")
+    println("Without them, it is difficult to progress.")
+    
+    // Demonstrate how collaboration can lead to success 
+    println("Collaboration is key to success. Working together with friends and partners can often achieve better results.")
+    
+    // Demonstrate how partnership can lead to creativity 
+    println("Partnership can also lead to creativity. Ideas and inspirations from friends and partners can make a project more successful.")
+    
+    // Message about the power of partnership
+    println(message)
+    
+    // Message at the end 
+    println("Thank you for participating in this program!")
+}
